a3869e7bd155c23123ded97a88056089ef970277
Djeser <92386703+jaspercuppen@users.noreply.github.com>
2025-01-07 22:41:47 +0100

    fully implemented the deletable armor layers

--- src/main/java/net/togyk/myneheroes/mixin/ArmorFeatureRendererMixin.java
+++ src/main/java/net/togyk/myneheroes/mixin/ArmorFeatureRendererMixin.java
@@ -25,58 +25,19 @@ import java.util.List;
 
 @Mixin(ArmorFeatureRenderer.class)
 public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
-    /*
-    this works partially
-    @Inject(
-            method = "renderArmor",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/entity/feature/ArmorFeatureRenderer;renderArmorParts(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/model/BipedEntityModel;ILnet/minecraft/util/Identifier;)V",
-                    ordinal = 0
-            ),
-            locals = LocalCapture.CAPTURE_FAILEXCEPTION
-    )
-    private void injectModifyLayerColor(MatrixStack matrixStack, VertexConsumerProvider consumerProvider, LivingEntity entity, EquipmentSlot equipmentSlot, int integer, BipedEntityModel model, CallbackInfo ci, ItemStack stack, ArmorItem armorItem, boolean bl, ArmorMaterial armorMaterial, int i, Iterator iterator, ArmorMaterial.Layer layer, int j) {
-        // Modify the local variable `j`
-        if (stack.getItem() instanceof DyeableAdvancedArmorItem advancedArmorItem && layer.isDyeable()) {
-            List<ArmorMaterial.Layer> layers = armorMaterial.layers();
-            j = advancedArmorItem.getColor(stack, layers.indexOf(layer));
-            MyneHeroes.LOGGER.info("{}?",j);
-        }
-    }
-    Should work but doesn't
+    //change the color of a armorMaterial layer based on the items NBT and if it extends the DyeableAdvancedArmorItem from this mod
+    //only works on dyeable layers so check your armorMaterial
     @ModifyVariable(
             method = "renderArmor",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/entity/feature/ArmorFeatureRenderer;renderArmorParts(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/model/BipedEntityModel;ILnet/minecraft/util/Identifier;)V",
-                    ordinal = 0
-            ),
+            at = @At("STORE"),
             ordinal = 2
     )
-    private int modifyColor(int j, @Local ItemStack itemStack, @Local ArmorMaterial.Layer layer) {
+    private int modifyColor(int i, @Local ItemStack itemStack, @Local ArmorMaterial.Layer layer) {
         if (layer.isDyeable() && itemStack.getItem() instanceof DyeableAdvancedArmorItem armorItem) {
-            //getting the layers of the armorMaterial
             List<ArmorMaterial.Layer> layers = armorItem.getMaterial().value().layers();
-            MyneHeroes.LOGGER.info("{}",armorItem.getColor(itemStack, layers.indexOf(layer)));
-            return armorItem.getColor(itemStack, layers.indexOf(layer));
+            return armorItem.getColor(itemStack,layers.indexOf(layer));
         } else {
-            return j;
+            return i;
         }
-    }
-     */
-    //Almost works
-    @ModifyVariable(
-            method = "renderArmor",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/entity/feature/ArmorFeatureRenderer;renderArmorParts(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/model/BipedEntityModel;ILnet/minecraft/util/Identifier;)V",
-                    ordinal = 0
-            ),
-            ordinal = 2
-    )
-    private int modifyColor(int j, @Local ItemStack itemStack, @Local ArmorMaterial.Layer layer) {
-        MyneHeroes.LOGGER.info("{}",j);
-        return -6265536;
     }
 }
