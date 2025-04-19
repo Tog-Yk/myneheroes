2974fe546ed6d1a69646884253e6dec9594dd0f7
Djeser <92386703+jaspercuppen@users.noreply.github.com>
2025-04-19 13:03:16 +0200

    fixed the anvil bug A BIT. made the upgrade only consume 1 item

--- src/main/java/net/togyk/myneheroes/Item/custom/UpgradeItem.java
+++ src/main/java/net/togyk/myneheroes/Item/custom/UpgradeItem.java
@@ -4,21 +4,27 @@ import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.togyk.myneheroes.ability.Ability;
 
+import java.util.List;
+
 public class UpgradeItem extends Item {
-    private final Ability ability;
+    private final List<Ability> abilities;
     private final int levelCost;
     private final ArmorItem.Type equipmentSlot;
 
     public UpgradeItem(Ability ability, int levelCost, ArmorItem.Type slot ,Settings settings) {
         super(settings);
 
-        this.ability = ability;
+        this.abilities = List.of(ability);
         this.levelCost = levelCost;
         this.equipmentSlot = slot;
     }
 
-    public Ability getAbility() {
-        return ability;
+    public UpgradeItem(List<Ability> abilities, int levelCost, ArmorItem.Type slot ,Settings settings) {
+        super(settings);
+
+        this.abilities = abilities;
+        this.levelCost = levelCost;
+        this.equipmentSlot = slot;
     }
 
     public int getLevelCost() {
@@ -27,5 +33,9 @@ public class UpgradeItem extends Item {
 
     public ArmorItem.Type getEquipmentSlot() {
         return equipmentSlot;
+    }
+
+    public List<Ability> getAbilities() {
+        return abilities;
     }
 }
--- src/main/java/net/togyk/myneheroes/mixin/CustomAnvilMixin.java
+++ src/main/java/net/togyk/myneheroes/mixin/CustomAnvilMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.ForgingScreenHandler;
 import net.minecraft.screen.Property;
+import net.minecraft.util.Identifier;
 import net.togyk.myneheroes.Item.custom.AdvancedArmorItem;
 import net.togyk.myneheroes.Item.custom.UpgradeItem;
 import net.togyk.myneheroes.MyneHeroes;
@@ -18,11 +19,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class CustomAnvilMixin {
     @Shadow @Final public static int INPUT_1_ID;
     @Shadow @Final public static int INPUT_2_ID;
+
+    @Shadow private int repairItemUsage;
 
     @Shadow @Final private Property levelCost;
 
@@ -34,32 +38,37 @@ public abstract class CustomAnvilMixin {
 
         // Check that both items are present
         if (!leftStack.isEmpty() && !rightStack.isEmpty()) {
-            if (leftStack.getItem() instanceof AdvancedArmorItem armor && rightStack.getItem() instanceof UpgradeItem upgradeItem && upgradeItem.getAbility() != null) {
-                MyneHeroes.LOGGER.info("heelpp");
+            if (leftStack.getItem() instanceof AdvancedArmorItem armor && rightStack.getItem() instanceof UpgradeItem upgradeItem && !upgradeItem.getAbilities().isEmpty()) {
                 if (upgradeItem.getEquipmentSlot() == null || upgradeItem.getEquipmentSlot() == armor.getType()) {
-                    MyneHeroes.LOGGER.info("cry about it");
                     // Create a copy to work with the result item
                     ItemStack resultStack = leftStack.copy();
 
-                    Ability ability = upgradeItem.getAbility();
+                    List<Ability> abilities = upgradeItem.getAbilities();
+                    List<Ability> armorAbilities = armor.getAbilities(leftStack);
+                    List<Identifier> armorAbilitiesIds = armorAbilities.stream().map(Ability::getId).toList();
+
+                    MyneHeroes.LOGGER.info(armorAbilities.toString());
+                    MyneHeroes.LOGGER.info(armorAbilitiesIds.toString());
 
 
-                    MyneHeroes.LOGGER.info(armor.getAbilities(leftStack).toString());
-                    MyneHeroes.LOGGER.info(ability.getId().toString());
-
-                    if (!armor.getAbilities(leftStack).stream().map(Ability::getId).toList().contains(ability.getId())) {
-                        MyneHeroes.LOGGER.info("seek therapy");
-                        armor.saveAbility(resultStack, ability);
-                        // Set the output to your new custom item
-                        CraftingResultInventory resultInventory = ((ForgingScreenHandlerAccessor) (Object) this).getOutput();
-                        resultInventory.setStack(0, resultStack);
-                        this.levelCost.set(upgradeItem.getLevelCost());
-
-                        ((AnvilScreenHandler) (Object) this).sendContentUpdates();
-
-                        ci.cancel(); // prevent further vanilla processing
+                    for (Ability ability : abilities) {
+                        MyneHeroes.LOGGER.info(String.valueOf(armorAbilitiesIds.contains(ability.getId())));
+                        if (!armorAbilitiesIds.contains(ability.getId())) {
+                            armor.saveAbility(resultStack, ability);
+                        }
                     }
+
+                    // Set the output to your new custom item
+                    CraftingResultInventory resultInventory = ((ForgingScreenHandlerAccessor) (Object) this).getOutput();
+                    resultInventory.setStack(0, resultStack);
+
+                    levelCost.set(upgradeItem.getLevelCost());
+
+                    repairItemUsage = 1;
+
+                    resultInventory.markDirty();
                 }
+                ci.cancel(); // prevent further vanilla processing
             }
         }
     }
