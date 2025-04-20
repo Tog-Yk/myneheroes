6bae7675c3b7c094cccef1ae0d7a0dfc73d47b93
Djeser <92386703+jaspercuppen@users.noreply.github.com>
2025-04-20 17:42:24 +0200

    updated StationaryArmor

--- src/main/java/net/togyk/myneheroes/entity/StationaryArmorEntity.java
+++ src/main/java/net/togyk/myneheroes/entity/StationaryArmorEntity.java
@@ -1,10 +1,12 @@
 package net.togyk.myneheroes.entity;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
@@ -12,9 +14,14 @@ import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import net.togyk.myneheroes.Item.custom.AdvancedArmorItem;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class StationaryArmorEntity extends ArmorStandEntity {
     private static final TrackedData<Boolean> isArmoring = DataTracker.registerData(StationaryArmorEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
@@ -140,5 +147,112 @@ public class StationaryArmorEntity extends ArmorStandEntity {
 
         // Equip the new armor
         player.equipStack(armorSlot, newArmor);
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        if (this.isRemoved()) {
+            return false;
+        } else {
+            World var4 = this.getWorld();
+            if (var4 instanceof ServerWorld) {
+                ServerWorld serverWorld = (ServerWorld)var4;
+                if (source.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY)) {
+                    this.kill();
+                    return false;
+                } else if (!this.isInvulnerableTo(source) && !this.isInvulnerable()) {
+                    if (source.isIn(DamageTypeTags.IS_EXPLOSION)) {
+                        this.playBreakSound();
+                        this.dropArmor();
+                        this.kill();
+                        return false;
+                    } else if (source.isIn(DamageTypeTags.IGNITES_ARMOR_STANDS)) {
+                        if (this.isOnFire()) {
+                            this.updateHealth(serverWorld, source, 0.15F);
+                        } else {
+                            this.setOnFireFor(5.0F);
+                        }
+
+                        return false;
+                    } else if (source.isIn(DamageTypeTags.BURNS_ARMOR_STANDS) && this.getHealth() > 0.5F) {
+                        this.updateHealth(serverWorld, source, 4.0F);
+                        return false;
+                    } else {
+                        boolean bl = source.isIn(DamageTypeTags.CAN_BREAK_ARMOR_STAND);
+                        boolean bl2 = source.isIn(DamageTypeTags.ALWAYS_KILLS_ARMOR_STANDS);
+                        if (!bl && !bl2) {
+                            return false;
+                        } else {
+                            Entity attacker = source.getAttacker();
+                            if (attacker instanceof PlayerEntity playerEntity) {
+                                if (!playerEntity.getAbilities().allowModifyWorld) {
+                                    return false;
+                                }
+                            }
+
+                            if (source.isSourceCreativePlayer()) {
+                                this.playBreakSound();
+                                this.dropArmor();
+                                this.kill();
+                                return true;
+                            } else {
+                                long l = serverWorld.getTime();
+                                if (l - this.lastHitTime > 5L && !bl2) {
+                                    serverWorld.sendEntityStatus(this, (byte)32);
+                                    this.emitGameEvent(GameEvent.ENTITY_DAMAGE, source.getAttacker());
+                                    this.lastHitTime = l;
+                                } else {
+                                    this.playBreakSound();
+                                    this.dropArmor();
+                                    this.kill();
+                                }
+
+                                return true;
+                            }
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private void updateHealth(ServerWorld world, DamageSource damageSource, float amount) {
+        float f = this.getHealth();
+        f -= amount;
+        if (f <= 0.5F) {
+            this.dropArmor();
+            this.kill();
+        } else {
+            this.setHealth(f);
+            this.emitGameEvent(GameEvent.ENTITY_DAMAGE, damageSource.getAttacker());
+        }
+
+    }
+
+    private void playBreakSound() {
+        this.getWorld().playSound((PlayerEntity)null, this.getX(), this.getY(), this.getZ(), this.getDeathSound(), this.getSoundCategory(), 1.0F, 1.0F);
+    }
+
+    @Override
+    protected @Nullable SoundEvent getDeathSound() {
+        ItemStack helmet = this.getEquippedStack(EquipmentSlot.HEAD);
+        if (helmet.getItem() instanceof AdvancedArmorItem item) {
+            return item.getMaterial().value().equipSound().value();
+        }
+        return super.getDeathSound();
+    }
+
+    @Override
+    public boolean shouldHideBasePlate() {
+        return true;
+    }
+
+    @Override
+    public ItemStack getPickBlockStack() {
+        return ItemStack.EMPTY;
     }
 }
