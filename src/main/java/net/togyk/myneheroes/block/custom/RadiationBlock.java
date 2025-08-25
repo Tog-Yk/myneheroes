3b8f39c23fb60d78c044096c955664fc2de8f1a4
Djeser <jasper.cuppen@gmail.com>
2025-08-25 23:03:21 +0200

    fixed radiation on generated blocks (sortof)

--- src/main/java/net/togyk/myneheroes/block/custom/RadiationBlock.java
+++ src/main/java/net/togyk/myneheroes/block/custom/RadiationBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.random.Random;
@@ -14,6 +15,8 @@ import net.togyk.myneheroes.effect.ModEffects;
 import java.util.List;
 
 public class RadiationBlock extends Block {
+    public static final BooleanProperty TICKING = BooleanProperty.of("ticking");
+
     public RadiationBlock(Settings settings) {
         super(settings);
     }
@@ -35,10 +38,21 @@ public class RadiationBlock extends Block {
     }
 
     @Override
+    protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        super.randomTick(state, world, pos, random);
+        if (!state.get(TICKING)) {
+            // schedule tick
+            world.scheduleBlockTick(pos, this, 24);
+            world.setBlockState(pos, state.with(TICKING, true));
+        }
+    }
+
+    @Override
     protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
         super.onBlockAdded(state, world, pos, oldState, notify);
         if (!world.isClient) {
             world.scheduleBlockTick(pos, this, 24); // start ticking
+            world.setBlockState(pos, state.with(TICKING, true));
         }
     }
 }
