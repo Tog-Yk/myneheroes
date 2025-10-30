13d7fc96e8f8e3772884b7065bccaf8ba375b7ac
Tog_yk <92386703+Tog-Yk@users.noreply.github.com>
2025-10-30 11:34:00 +0100

    fabric loom 1.7-SNAPSHOT -> 1.10.5

--- src/main/java/net/togyk/myneheroes/worldgen/ModBiomeModifications.java
+++ src/main/java/net/togyk/myneheroes/worldgen/ModBiomeModifications.java
@@ -2,32 +2,42 @@ package net.togyk.myneheroes.worldgen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import net.togyk.myneheroes.MyneHeroes;
 
 public class ModBiomeModifications {
+    public static final RegistryKey<PlacedFeature> VIBRANIUM_ORE_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MyneHeroes.MOD_ID, "vibranium_ore_placed"));
+    public static final RegistryKey<PlacedFeature> TITANIUM_ORE_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MyneHeroes.MOD_ID, "titanium_ore_placed"));
+    public static final RegistryKey<PlacedFeature> URANIUM_ORE_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MyneHeroes.MOD_ID, "uranium_ore_placed"));
+
+    public static final RegistryKey<PlacedFeature> METEOR_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MyneHeroes.MOD_ID, "meteor_placed"));
+
     public static void registerModBiomeModifications(){
         MyneHeroes.LOGGER.info("Registering Biome Modifacations for " + MyneHeroes.MOD_ID);
         BiomeModifications.addFeature(
                 BiomeSelectors.foundInOverworld(),
                 GenerationStep.Feature.UNDERGROUND_ORES,
-                ModPlacedFeatures.VIBRANIUM_ORE_KEY
+                VIBRANIUM_ORE_KEY
         );
         BiomeModifications.addFeature(
                 BiomeSelectors.foundInOverworld(),
                 GenerationStep.Feature.UNDERGROUND_ORES,
-                ModPlacedFeatures.TITANIUM_ORE_KEY
+                TITANIUM_ORE_KEY
         );
         BiomeModifications.addFeature(
                 BiomeSelectors.foundInOverworld(),
                 GenerationStep.Feature.UNDERGROUND_ORES,
-                ModPlacedFeatures.URANIUM_ORE_KEY
+                URANIUM_ORE_KEY
         );
 
         BiomeModifications.addFeature(
                 BiomeSelectors.foundInOverworld(),
                 GenerationStep.Feature.UNDERGROUND_DECORATION,
-                ModPlacedFeatures.METEOR_KEY
+                METEOR_KEY
         );
         //add more placed features here */
     }
