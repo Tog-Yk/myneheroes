82ea82573b4cf7f8110ac5cc2d933046550c4435
Tog_yk <92386703+Tog-Yk@users.noreply.github.com>
2025-10-31 12:57:50 +0100

    You need to charge an Injection

--- src/main/java/net/togyk/myneheroes/Item/custom/PowerInjectionItem.java
+++ src/main/java/net/togyk/myneheroes/Item/custom/PowerInjectionItem.java
@@ -8,6 +8,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
@@ -37,33 +38,70 @@ public class PowerInjectionItem extends Item implements UpgradableItem {
     }
 
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        Power power = this.getPower(user.getStackInHand(hand));
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (getPullProgress(this.getMaxUseTime(stack, user) - remainingUseTicks) == 1.0f && user instanceof PlayerEntity player) {
+            Power power = this.getPower(stack);
 
-        List<Power> powers = PowerData.getPowers(user);
-        List<Identifier> powerIds = new ArrayList<>();
-        if (!powers.isEmpty()) {
-            powerIds = powers.stream().map(Power::getId).toList();
-        }
-        if (power == null && !powers.isEmpty()) {
-            int scrolled = ScrollData.getScrolledPowersOffset(user);
-            if (powers.size() > scrolled) {
-                Power usersPower = powers.get(scrolled);
-                this.setPower(user.getStackInHand(hand), usersPower);
-                PowerData.removePower(user, usersPower);
-                user.swingHand(hand);
-                return TypedActionResult.success(user.getStackInHand(hand));
+            List<Power> powers = PowerData.getPowers(player);
+            List<Identifier> powerIds = new ArrayList<>();
+            if (!powers.isEmpty()) {
+                powerIds = powers.stream().map(Power::getId).toList();
             }
-        } else if (power != null && !powerIds.contains(power.id)) {
-            if (world.getGameRules().getBoolean(ModGamerules.GIVE_POWERS_ABOVE_LIMIT) || powers.size() < world.getGameRules().getInt(ModGamerules.POWER_LIMIT)) {
-                PowerData.addPower(user, power);
-                this.setPower(user.getStackInHand(hand), null);
-                user.swingHand(hand);
-                return TypedActionResult.success(user.getStackInHand(hand));
+            if (power == null && !powers.isEmpty()) {
+                int scrolled = ScrollData.getScrolledPowersOffset(player);
+                if (powers.size() > scrolled) {
+                    Power usersPower = powers.get(scrolled);
+                    this.setPower(stack, usersPower);
+                    PowerData.removePower(player, usersPower);
+                }
+            } else if (power != null && !powerIds.contains(power.id)) {
+                if (world.getGameRules().getBoolean(ModGamerules.GIVE_POWERS_ABOVE_LIMIT) || powers.size() < world.getGameRules().getInt(ModGamerules.POWER_LIMIT)) {
+                    PowerData.addPower(player, power);
+                    this.setPower(stack, null);
+                }
             }
+
+            player.incrementStat(Stats.USED.getOrCreateStat(this));
         }
-        return TypedActionResult.pass(user.getStackInHand(hand));
     }
+
+    public static float getPullProgress(int useTicks) {
+        float f = useTicks / 20.0F;
+        f = (f * f + f * 2.0F) / 3.0F;
+        if (f > 1.0F) {
+            f = 1.0F;
+        }
+
+        return f;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        Power power = this.getPower(itemStack);
+        List<Power> powers = PowerData.getPowers(user);
+
+        boolean canTake = power == null && !powers.isEmpty();
+        boolean canGive = power != null && world.getGameRules().getBoolean(ModGamerules.GIVE_POWERS_ABOVE_LIMIT) || powers.size() < world.getGameRules().getInt(ModGamerules.POWER_LIMIT);
+        if (!(canTake || canGive)) {
+            return TypedActionResult.fail(itemStack);
+        } else {
+            user.setCurrentHand(hand);
+            return TypedActionResult.consume(itemStack);
+        }
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 72000;
+    }
+
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+
     @Override
     public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
         if (type.isAdvanced()) {
