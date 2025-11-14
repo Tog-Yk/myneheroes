2efb4d57dd62cf7b6db3889bcdd1e0ba1afcd063
Djeser <jasper.cuppen@gmail.com>
2025-11-15 00:35:03 +0100

    built-in resource pack for adamantium

--- src/main/java/net/togyk/myneheroes/resourcepack/ModResourcePacks.java
+++ src/main/java/net/togyk/myneheroes/resourcepack/ModResourcePacks.java
@@ -6,8 +6,12 @@ import net.togyk.myneheroes.MyneHeroes;
 
 public class ModResourcePacks {
     public static final ResourcePackInfo SHIBASHIS_SHENANIGANS = new ResourcePackInfo(Identifier.of(MyneHeroes.MOD_ID, "shibashis_shenanigans"), ResourcePackActivationType.NORMAL);
+    public static final ResourcePackInfo WHITER_ADAMANTIUM = new ResourcePackInfo(Identifier.of(MyneHeroes.MOD_ID, "whiter_adamantium"), ResourcePackActivationType.NORMAL);
 
     public static void registerResourcePacks() {
         SHIBASHIS_SHENANIGANS.register();
+        //TODO access if it the resource pack should be the default textures
+        //if the textures get swaped the new name should be: Distinct/Alternative/Exotic adamantium
+        WHITER_ADAMANTIUM.register();
     }
 }
