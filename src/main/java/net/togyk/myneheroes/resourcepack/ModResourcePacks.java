4fdfb2eb4853c97aac5021d8999e94b1588f6519
Djeser <jasper.cuppen@gmail.com>
2026-01-02 16:05:37 +0100

    better texture packs

--- src/main/java/net/togyk/myneheroes/resourcepack/ModResourcePacks.java
+++ src/main/java/net/togyk/myneheroes/resourcepack/ModResourcePacks.java
@@ -6,12 +6,14 @@ import net.togyk.myneheroes.MyneHeroes;
 
 public class ModResourcePacks {
     public static final ResourcePackInfo SHIBASHIS_SHENANIGANS = new ResourcePackInfo(Identifier.of(MyneHeroes.MOD_ID, "shibashis_shenanigans"), ResourcePackActivationType.NORMAL);
-    public static final ResourcePackInfo WHITER_ADAMANTIUM = new ResourcePackInfo(Identifier.of(MyneHeroes.MOD_ID, "whiter_adamantium"), ResourcePackActivationType.NORMAL);
+    public static final ResourcePackInfo ALTERNATIVE_ADAMANTIUM = new ResourcePackInfo(Identifier.of(MyneHeroes.MOD_ID, "alternative_adamantium"), ResourcePackActivationType.NORMAL);
+    public static final ResourcePackInfo DISTINCT_ADAMANTIUM = new ResourcePackInfo(Identifier.of(MyneHeroes.MOD_ID, "distinct_adamantium"), ResourcePackActivationType.NORMAL);
 
     public static void registerResourcePacks() {
         SHIBASHIS_SHENANIGANS.register();
-        //TODO access if it the resource pack should be the default textures
+        //TODO access if any of the resource packs should be the default textures
         //if the textures get swaped the new name should be: Distinct/Alternative/Exotic adamantium
-        WHITER_ADAMANTIUM.register();
+        ALTERNATIVE_ADAMANTIUM.register();
+        DISTINCT_ADAMANTIUM.register();
     }
 }
