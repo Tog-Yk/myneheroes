9933afdbb3ef0b57c74707a374bf8aebb9a79c0e
Tog_Yk <jasper.cuppen@gmail.com>
2026-03-09 13:40:07 +0100

    missing class from 4 commits ago

---
+++ src/main/java/net/togyk/myneheroes/Item/custom/HotbarAbilityHoldingUpgradeItem.java
@@ -0,0 +1,61 @@
+package net.togyk.myneheroes.Item.custom;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.ClickType;
+import net.togyk.myneheroes.ability.Ability;
+import net.togyk.myneheroes.component.ModDataComponentTypes;
+import net.togyk.myneheroes.upgrade.AbilityUpgrade;
+import net.togyk.myneheroes.upgrade.Upgrade;
+import net.togyk.myneheroes.util.AbilityUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotbarAbilityHoldingUpgradeItem extends UpgradeItem implements AbilityHolding, UpgradeHolding {
+    public HotbarAbilityHoldingUpgradeItem(Upgrade upgrade, Item.Settings settings) {
+        super(upgrade, settings);
+    }
+
+    @Override
+    public List<Ability> getHotbarAbilities(ItemStack stack) {
+        List<Ability> abilities = new ArrayList<>();
+
+        Upgrade upgrade = this.getUpgrade(stack, null);
+        if (upgrade instanceof AbilityUpgrade abilityUpgrade) {
+            for (Ability ability : abilityUpgrade.getAbilities()) {
+                ability.setHolder(upgrade);
+                abilities.add(ability);
+            }
+        }
+
+        return abilities;
+    }
+
+    @Override
+    public void saveAbility(ItemStack stack, Ability ability) {
+    }
+
+    public Upgrade getUpgrade(ItemStack stack, ItemStack upgradeableStack) {
+        NbtCompound nbt = stack.getOrDefault(ModDataComponentTypes.UPGRADES, new NbtCompound());
+
+        Upgrade upgrade = super.getUpgrade(stack, upgradeableStack);
+        if (nbt.contains("upgrade")) {
+            upgrade = AbilityUtil.nbtToUpgrade(nbt.getCompound("upgrade"));
+        }
+        upgrade.setHolderStack(stack);
+        return upgrade;
+    }
+
+    @Override
+    public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
+        if (this.getUpgrade(stack, null).onClicked(stack, otherStack, slot, clickType, player, cursorStackReference)) {
+            return true;
+        }
+        return super.onClicked(stack, otherStack, slot, clickType, player, cursorStackReference);
+    }
+}
